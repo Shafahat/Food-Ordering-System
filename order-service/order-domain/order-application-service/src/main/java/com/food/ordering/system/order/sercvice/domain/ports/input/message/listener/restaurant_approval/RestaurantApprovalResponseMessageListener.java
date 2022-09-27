04067a5e9bcaf596cd453accb41b5a120e2dc661
Shafahat <ssardarov2020@ada.edu.az>
2022-09-27 10:53:01 +0400

    Fixed folder name to service

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/sercvice/domain/ports/input/message/listener/restaurant_approval/RestaurantApprovalResponseMessageListener.java
+++
@@ -1,9 +0,0 @@
-package com.food.ordering.system.order.sercvice.domain.ports.input.message.listener.restaurant_approval;
-
-import com.food.ordering.system.order.sercvice.domain.dto.message.RestaurantApprovalResponse;
-
-public interface RestaurantApprovalResponseMessageListener {
-    void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse);
-
-    void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse);
-}
--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/sercvice/domain/entity/Customer.java
+++
@@ -1,7 +0,0 @@
-package com.food.ordering.system.order.sercvice.domain.entity;
-
-import com.food.ordering.system.domain.entity.AggregateRoot;
-import com.food.ordering.system.domain.valueobject.CustomerId;
-
-public class Customer extends AggregateRoot<CustomerId> {
-}
