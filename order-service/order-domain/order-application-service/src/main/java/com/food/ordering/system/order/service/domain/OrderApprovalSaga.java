54a60479020bcab5b0439291badf6d37651a675d
Shafahat <ssardarov2020@ada.edu.az>
2022-10-05 01:25:39 +0400

    completed saga flow for order service

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderApprovalSaga.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderApprovalSaga.java
@@ -2,9 +2,9 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.domain.event.DomainEvent;
 import com.food.ordering.system.domain.event.EmptyEvent;
-import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
 import com.food.ordering.system.payment.service.domain.entity.Order;
-import com.food.ordering.system.payment.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.payment.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.saga.SagaStep;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,30 +14,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class OrderPaymentSaga implements SagaStep<PaymentResponse, OrderPaidEvent, DomainEvent> {
+public class OrderApprovalSaga implements SagaStep<RestaurantApprovalResponse, DomainEvent, OrderCancelledEvent> {
     private final OrderDomainService service;
     private final OrderSagaHelper helper;
 
     @Override
     @Transactional
-    public OrderPaidEvent process(PaymentResponse response) {
-        log.info("Completing payment for order wth id: {}", response.getOrderId());
+    public DomainEvent process(RestaurantApprovalResponse response) {
+        log.info("Approving order with id: {}", response.getOrderId());
         Order order = helper.findOrder(response.getOrderId());
-        OrderPaidEvent event = service.payOrder(order);
+        service.approveOrder(order);
         helper.saveOrder(order);
-        log.info("Order with id: {} is paid", event.getOrder().getId().getValue());
-        return event;
+        log.info("Order with id: {} is approved", order.getId().getValue());
+        return new EmptyEvent();
     }
 
     @Override
     @Transactional
-    public DomainEvent<?> rollback(PaymentResponse response) {
+    public OrderCancelledEvent rollback(RestaurantApprovalResponse response) {
         log.info("Cancelling order with id: {}", response.getOrderId());
         Order order = helper.findOrder(response.getOrderId());
-        service.cancelOrder(order, response.getFailureMessages());
+        OrderCancelledEvent event = service.cancelOrderPayment(order, response.getFailureMessages());
         helper.saveOrder(order);
-        log.info("Order with id: {} is cancelled", order.getId().getValue());
-        return new EmptyEvent();
+        log.info("Order with id: {} is cancelling", event.getOrder().getId().getValue());
+        return event;
     }
-
 }
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
@@ -2,6 +2,7 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurant_approval.RestaurantApprovalResponseMessageListener;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -9,14 +10,19 @@ import org.springframework.validation.annotation.Validated;
 @Slf4j
 @Validated
 @Service
+@RequiredArgsConstructor
 public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
-    @Override
-    public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
+    private final OrderApprovalSaga approvalSaga;
 
+    @Override
+    public void orderApproved(RestaurantApprovalResponse response) {
+        approvalSaga.process(response);
+        log.info("Order Approved: {}", response);
     }
 
     @Override
-    public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
-
+    public void orderRejected(RestaurantApprovalResponse response) {
+        approvalSaga.rollback(response);
+        log.info("Order Rejected: {}", response);
     }
 }
