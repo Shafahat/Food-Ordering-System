fb6fa44d9d126505eef4c9b1e71a47318f2ae1d7
Shafahat <ssardarov2020@ada.edu.az>
2022-10-08 16:42:54 +0400

    optimized imports

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurant_approval/RestaurantApprovalRequestMessagePublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurant_approval/RestaurantApprovalRequestMessagePublisher.java
@@ -1,7 +1,6 @@
 package com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurant_approval;
 
 import com.food.ordering.system.order.service.domain.outbox.model.approval.OrderApprovalOutboxMessage;
-import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
 import com.food.ordering.system.outbox.OutboxStatus;
 
 import java.util.function.BiConsumer;
